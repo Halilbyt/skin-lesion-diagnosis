35cc126b9f059e727b47b8d001b641886490139a
Halil İbrahim BAYAT <halilbayat@Halil-MacBook-Pro.local>
2026-04-20 19:50:49 +0300

    route and core api parts added

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
+from app.api.routes import router as api_routher
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(CURRENT_DIR)
+FRONTEND_DIR = os.path.join(PROJECT_ROOT,"frontend")
+
+app = FastAPI(title="Skin Lesion Diagnosis API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
